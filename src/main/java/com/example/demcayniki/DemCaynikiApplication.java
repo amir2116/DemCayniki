4fe71b9bae9b315c0ba0498bc3770529cf9a9007
amir.aliyev <amir.aliyev@digismart.az>
2026-01-07 14:29:13 +0400

    sifte

---
+++ src/main/java/com/example/demcayniki/DemCaynikiApplication.java
@@ -0,0 +1,13 @@
+package com.example.demcayniki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemCaynikiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemCaynikiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demcayniki/DemCaynikiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demcayniki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemCaynikiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
