2dc8a46c23364dff370e904ae0f5b34785083f06
AmirEv <amiraliyev@gmail.com>
2026-01-22 21:04:41 +0400

    d

--- src/main/java/com/example/demcayniki/config/SecurityConfig.java
+++ src/main/java/com/example/demcayniki/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.header.writers.ReferrerPolicyHeaderWriter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
@@ -23,10 +24,19 @@ public class SecurityConfig {
 
         http.headers(headers -> headers
                 .contentTypeOptions(Customizer.withDefaults())
-////                .referrerPolicy(ref ->
-//                        ref.policy(ReferrerPolicy
-                        );;
+                .referrerPolicy(ref ->
+                        ref.policy(ReferrerPolicyHeaderWriter.ReferrerPolicy.NO_REFERRER)
+                        )
+                .httpStrictTransportSecurity(htps -> htps
+                        .includeSubDomains(true)
+                        .preload(true)
+                        .maxAgeInSeconds(31536000))
+        );
 
-        http.authorizeHttpRequests(authorizeRequests -> authorizeRequests.anyRequest().authenticated());
-    return http.build();}
+        http.authorizeHttpRequests(authorizeRequests -> authorizeRequests
+
+                .anyRequest().authenticated());
+
+        return http.build();
+    }
 }
