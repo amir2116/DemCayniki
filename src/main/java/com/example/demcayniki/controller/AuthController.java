8641f0a4908fdf7916933ca424283c34f90b7458
AmirEv <amiraliyev@gmail.com>
2026-01-31 12:40:44 +0400

    d

--- src/main/java/com/example/demcayniki/controller/AuthController.java
+++ src/main/java/com/example/demcayniki/controller/AuthController.java
@@ -1,4 +1,26 @@
 package com.example.demcayniki.controller;
 
+import com.example.demcayniki.model.requests.AuthRequest;
+import com.example.demcayniki.service.AuthService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
 public class AuthController {
+
+    @Autowired
+    private AuthService authService;
+
+    public ResponseEntity<String> login(@Valid @RequestBody AuthRequest authRequest) {
+        Authentication auth = authService.authenticate(authRequest.getEmail(), authRequest.getPassword());
+        return ResponseEntity.ok("LOGIN OK: " + auth.getName());
+    }
+
 }
---
+++ src/main/java/com/example/demcayniki/service/AuthService.java
@@ -0,0 +1,20 @@
+package com.example.demcayniki.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    public Authentication authenticate(String username, String password) {
+        return authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(username, password)
+        );
+    }
+}
