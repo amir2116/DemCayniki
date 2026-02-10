c2df556b4795ced144e6a7c9d3efa8c71d9d3968
amir.aliyev <amir.aliyev@digismart.az>
2026-02-10 11:51:22 +0400

    Security Bashlandi

--- src/main/java/com/example/demcayniki/security/SecurityConfig.java
+++ src/main/java/com/example/demcayniki/security/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.example.demcayniki.config;
+package com.example.demcayniki.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
--- src/main/java/com/example/demcayniki/security/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/example/demcayniki/security/jwt/JwtAuthenticationFilter.java
@@ -1,4 +1,35 @@
 package com.example.demcayniki.security.jwt;
 
-public class JwtAuthenticationFilter {
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Objects;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+  private final JWTService jwtService;
+
+  public JwtAuthenticationFilter(JWTService jwtService) {
+    this.jwtService = Objects.requireNonNull(jwtService,  "jwtService must not be null");
+  }
+
+  @Override
+  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+      throws ServletException, IOException {
+
+    String header = request.getHeader("Authorization");
+    if (header == null || !header.startsWith("Bearer ")) {
+      filterChain.doFilter(request, response);
+    }
+
+    String token = header.substring("7".length()).trim();
+    if (token.isEmpty()) {
+      filterChain.doFilter(request, response);
+    }
+
+
+  }
 }
